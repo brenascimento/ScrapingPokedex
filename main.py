bc1686a7abd42a1e34c53e9e5048a94374d8cc8a
BrennoNasciment <>
2022-02-18 23:57:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+# import requests
+
+# to save html file and don't make much requests
+# req = requests.get('https://pokemondb.net/pokedex/all')
+# 
+# with open('pokedex.html', 'w') as file:
+#   file.write(req.text)
+
+with open('pokedex.html') as f:
+  soup = BeautifulSoup(f, 'html.parser')
+
+pokedex_table = soup.find(id="pokedex").find("tbody")
+# for poke in pokedex_table.find_all("tr")[0:5]:
+#   print(len(poke), type(poke))
+#   print(poke.find(class_="infocard-cell-data"))
+
+def pokemon_stat(pokedex):
+  pokemons = []
+  for tr in pokedex.find_all("tr"):
+    p_id = tr.select_one("td .infocard-cell-data").string
+    name = tr.select_one(".cell-name a").string
+    types = [i.string for i in tr.select(".cell-icon a")]
+    total = tr.select_one('.cell-total').string
+    hp, attack, defense, special_attack, special_defense, speed =\
+      map(lambda x: x.string, tr.select('.cell-total~.cell-num'))
+
+
+    pokemon = {"p_id": int(p_id),
+      "name":name,
+      "p_type":types, 
+      "total_attr":total,
+      "hp":hp,
+      "attack":attack,
+      "defense":defense,
+      "special_attack":special_attack,
+      "special_defense":special_defense,
+      "speed":speed}
+    pokemons.append(pokemon)
+  return pokemons
+
+df = pd.DataFrame(pokemon_stat(pokedex_table))
+df.to_csv('pokedex.csv', index=False)
+
+poke = pd.read_csv('pokedex.csv')
+print(poke.head())
+
+
